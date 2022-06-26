07d173854fc031871883c5ec0e623f24b417f6cc
Scott Wardle <swardle@gmail.com>
2022-06-26 10:04:49 -0700

    Rewrite math so Parametric Equation of an Ellipse

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,21 @@
-/**
- Copyright 2019 Itay Grudev
- License: MIT
- jQuery version: https://jsfiddle.net/ItayGrudev/3jqryc1m/1
- */
 
  window.addEventListener('load', function () {
     document.querySelectorAll( '.ciclegraph' ).forEach( ( ciclegraph )=>{
         let circles = ciclegraph.querySelectorAll( '.circle' )
-        let angle = 90, dangle = 360 / circles.length
+        let angle = 0, dangle = 360 / circles.length
         for( let i = 0; i < circles.length; ++i ){
             let circle = circles[i]
-            circle.style.transform = `rotate(${angle}deg) translate(${ciclegraph.clientWidth / 2}px) rotate(-${angle}deg)`
+
+            // Parametric Equation of an Ellipse
+            // x,y are the coordinates of any point on the ellipse,
+            // a, b are the radius on the x and y axes respectively, 
+            // t is the parameter, which ranges from 0 to 2π radians.
+            const t = angle * (Math.PI / 180);
+            const a = ciclegraph.clientWidth/2;
+            const b = ciclegraph.clientHeight/2;
+            const x = a * Math.sin(t);
+            const y = b * Math.cos(t);
+            circle.style.transform = `translate(${x}px, ${y}px)`
             angle += dangle
         }
     })  
