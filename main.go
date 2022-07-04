426400fdf988f669ea99c61e7afbe347b5e69b3d
Scott Wardle <swardle@gmail.com>
2022-07-04 16:57:17 -0700

    Make the table css look a bit better

--- main.go
+++ main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"runtime"
 )
 
 func main() {
+	isWin := false
+	if runtime.GOOS == "windows" {
+		isWin = true
+	}
+
+	// Disable log prefixes such as the default timestamp.
+	// Prefix text prevents the message from being parsed as JSON.
+	// A timestamp is added when shipping logs to Cloud Logging.
+	log.SetFlags(0)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		if isWin {
+			port = "8080"
+		} else {
+			port = "80"
+		}
+		log.Printf("Defaulting to port %s", port)
+	}
 
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
--- static/js/CircularTable.js
+++ static/js/CircularTable.js
@@ -1,21 +1,21 @@
 
  window.addEventListener('load', function () {
-    document.querySelectorAll( '.ciclegraph' ).forEach( ( ciclegraph )=>{
-        let circles = ciclegraph.querySelectorAll( '.circle' )
-        let angle = 0, dangle = 360 / circles.length
-        for( let i = 0; i < circles.length; ++i ){
-            let circle = circles[i]
+    document.querySelectorAll( '.CircularTable' ).forEach( ( circularTable )=>{
+        let players = circularTable.querySelectorAll( '.PlayerContainer' )
+        let angle = 0, dangle = 360 / players.length
+        for( let i = 0; i < players.length; ++i ){
+            let player = players[i]
 
             // Parametric Equation of an Ellipse
             // x,y are the coordinates of any point on the ellipse,
             // a, b are the radius on the x and y axes respectively, 
             // t is the parameter, which ranges from 0 to 2π radians.
             const t = angle * (Math.PI / 180);
-            const a = ciclegraph.clientWidth/2;
-            const b = ciclegraph.clientHeight/2;
+            const a = circularTable.clientWidth/2;
+            const b = circularTable.clientHeight/2;
             const x = a * Math.sin(t);
             const y = b * Math.cos(t);
-            circle.style.transform = `translate(${x}px, ${y}px)`
+            player.style.transform = `translate(${x}px, ${y}px)`
             angle += dangle
         }
     })  
